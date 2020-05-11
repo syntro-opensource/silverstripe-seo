1edc961b9531ebb05e7fefd95b57bf13eb026718
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-05-11 19:59:42 +0200

    CHANGE: add a meta report

---
+++ src/Reports/MissingMetaDescriptionReport.php
@@ -0,0 +1,41 @@
+<?php
+namespace Syntro\SEOMeta\Reports;
+
+use SilverStripe\Reports\Report;
+use SilverStripe\CMS\Model\SiteTree;
+
+/**
+ *
+ */
+class MissingMetaDescriptionReport extends Report
+{
+
+    public function title()
+    {
+        return _t(__CLASS__ . '.Title', 'Pages with missing meta description');
+    }
+
+    public function description()
+    {
+        return _t(__CLASS__ . '.Description', '<div class="alert alert-info">It is useful to set a meta description for each page. This helps with indexing and displaying the specific page by search engines.</div>');
+    }
+
+    public function sourceRecords()
+    {
+        return SiteTree::get()->filter([
+            'MetaDescription' => null
+        ]);
+    }
+
+    public function columns()
+    {
+        return [
+            "Title" => [
+                "title" => "Title", // todo: use NestedTitle(2)
+                "link" => true,
+            ],
+        ];
+    }
+
+
+}
