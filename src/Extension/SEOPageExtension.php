629829dfd9a21a417142e0c3fba0dd479c950edb
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-05-11 20:51:16 +0200

    CHANGE: only change fields if we actually have meta

--- src/Extension/SEOPageExtension.php
+++ src/Extension/SEOPageExtension.php
@@ -77,77 +77,76 @@ class SEOPageExtension extends DataExtension {
         // Move meta field to the new SEO-Tab
         $metaDescriptionField = $fields->dataFieldByName('MetaDescription');
         $metaExtraField = $fields->dataFieldByName('ExtraMeta');
-        $fields->dataFieldByName('Title')->setTargetLength($titleLength);
-        $fields->removeByName([
-            'MetaDescription',
-            'ExtraMeta',
-            'Metadata'
-        ]);
-        $fields->addFieldToTab(
-            'Root.SEO',
-            ToggleCompositeField::create(
-                'Metadata',
-                _t(__CLASS__.'.MetadataToggle', 'Metadata'),
-                [
+        if (!is_null($metaDescriptionField) && !is_null($metaExtraField)) {
+            $fields->dataFieldByName('Title')->setTargetLength($titleLength);
+            $fields->removeByName([
+                'MetaDescription',
+                'ExtraMeta',
+                'Metadata'
+            ]);
+            $fields->addFieldToTab(
+                'Root.SEO',
+                ToggleCompositeField::create(
+                    'Metadata',
+                    _t(__CLASS__.'.MetadataToggle', 'Metadata'),
+                    [
 
-                    $metaDefaultImage = UploadField::create('MetaImageDefault','Image'),
-                    $metaDescriptionField,
-                    $metaExtraField
-                ]
-            )->setHeadingLevel(4)
-        );
-        $metaDescriptionField->setTargetLength(125);
-        $metaDefaultImage
-            ->setReadOnly(true)
-            ->setValue($seoManager->getOGImage())
-            ->setRightTitle(_t(__CLASS__ . '.DefaultImageInfo', 'The Image which is currently displayed by crawlers. You can set a global default under "Settings" or add a custom image in the OpenGraph box.'));
+                        $metaDefaultImage = UploadField::create('MetaImageDefault','Image'),
+                        $metaDescriptionField,
+                        $metaExtraField
+                    ]
+                )->setHeadingLevel(4)
+            );
+            $metaDescriptionField->setTargetLength(125);
+            $metaDefaultImage
+                ->setReadOnly(true)
+                ->setValue($seoManager->getOGImage())
+                ->setRightTitle(_t(__CLASS__ . '.DefaultImageInfo', 'The Image which is currently displayed by crawlers. You can set a global default under "Settings" or add a custom image in the OpenGraph box.'));
 
 
-        // Add Opengraph Meta
-        $OGTypes = [];
-        foreach (OGMetaGenerator::config()->available_types as $value) {
-            $OGTypes[$value] = _t(OGMetaGenerator::class . '.'.$value, $value);
+            // Add Opengraph Meta
+            $OGTypes = [];
+            foreach (OGMetaGenerator::config()->available_types as $value) {
+                $OGTypes[$value] = _t(OGMetaGenerator::class . '.'.$value, $value);
+            }
+            $fields->addFieldToTab(
+                'Root.SEO',
+                ToggleCompositeField::create(
+                    'OpenGraph',
+                    _t(__CLASS__.'.OpenGraphToggle', 'OpenGraph SEO (Facebook)'),
+                    [
+                        $ogType = DropdownField::create('OGMetaType','Type',
+                            $OGTypes
+                        ),
+                        $ogTitle = TextField::create('OGMetaTitle','Title'),
+                        $ogImage = UploadField::create('OGMetaImage','Image'),
+                        $ogDescription = TextareaField::create('OGMetaDescription','Description'),
+                    ]
+                )->setHeadingLevel(4)
+            );
+            $ogTitle->setAttribute('placeholder', $owner->Title)->setTargetLength(50);
+            $ogDescription->setAttribute('placeholder', $owner->MetaDescription);
+
+
+            // Add Twitter Meta
+            $TwitterTypes = [];
+            foreach (TwitterMetaGenerator::config()->available_types as $value) {
+                $TwitterTypes[$value] = _t(TwitterMetaGenerator::class . '.'.$value, $value);
+            }
+            $fields->addFieldToTab(
+                'Root.SEO',
+                ToggleCompositeField::create(
+                    'Twitter',
+                    _t(__CLASS__.'.OpenGraphToggle', 'Twitter SEO'),
+                    [
+                        $twitterType = DropdownField::create('TwitterMetaType','Type',
+                            $TwitterTypes
+                        ),
+                        $twitterTitle = TextField::create('TwitterMetaCreator','Creator'),
+                    ]
+                )->setHeadingLevel(4)
+            );
         }
-        $fields->addFieldToTab(
-            'Root.SEO',
-            ToggleCompositeField::create(
-                'OpenGraph',
-                _t(__CLASS__.'.OpenGraphToggle', 'OpenGraph SEO (Facebook)'),
-                [
-                    $ogType = DropdownField::create('OGMetaType','Type',
-                        $OGTypes
-                    ),
-                    $ogTitle = TextField::create('OGMetaTitle','Title'),
-                    $ogImage = UploadField::create('OGMetaImage','Image'),
-                    $ogDescription = TextareaField::create('OGMetaDescription','Description'),
-                ]
-            )->setHeadingLevel(4)
-        );
-        $ogTitle->setAttribute('placeholder', $owner->Title)->setTargetLength(50);
-        $ogDescription->setAttribute('placeholder', $owner->MetaDescription);
-
-
-        // Add Twitter Meta
-        $TwitterTypes = [];
-        foreach (TwitterMetaGenerator::config()->available_types as $value) {
-            $TwitterTypes[$value] = _t(TwitterMetaGenerator::class . '.'.$value, $value);
-        }
-        $fields->addFieldToTab(
-            'Root.SEO',
-            ToggleCompositeField::create(
-                'Twitter',
-                _t(__CLASS__.'.OpenGraphToggle', 'Twitter SEO'),
-                [
-                    $twitterType = DropdownField::create('TwitterMetaType','Type',
-                        $TwitterTypes
-                    ),
-                    $twitterTitle = TextField::create('TwitterMetaCreator','Creator'),
-                ]
-            )->setHeadingLevel(4)
-        );
-
-
-
         return $fields;
     }
 
