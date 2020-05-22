5782d0e4d9d135f8b36386e904b57c56790d6a6b
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-05-22 20:16:34 +0200

    ADD: tests

---
+++ tests/php/Tags/TagTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Syntro\Seo\Tests\Tags;
+
+use SilverStripe\Dev\SapphireTest;
+
+class TagTest extends SapphireTest
+{
+    public function testRender()
+    {
+        $this->assertEquals(2, 1+1);
+    }
+}
