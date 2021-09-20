1bd1ee1d1e2032a36686bbe9124e6361fbd02ea8
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2021-09-20 18:52:47 +0200

    add: test setup

---
+++ tests/php/DummyTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Syntro\Silvershare\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+use Syntro\SilverShare\Dev\SharedObject;
+
+/**
+ * Dummy Test
+ *
+ * @author Matthias Leutenegger <hello@syntro.ch>
+ */
+class DummyTest extends SapphireTest
+{
+    /**
+     * testCMSFields
+     *
+     * @return void
+     */
+    public function testArith()
+    {
+        $this->assertEquals(2, 1+1);
+    }
+
+}
