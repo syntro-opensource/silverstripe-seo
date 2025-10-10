6d961631283c0c121e6e48f209f1d6ac09978fa5
Patrick Côté <git@ptct.ch>
2025-10-10 12:28:23 +0200

    testing testing

--- src/Extensions/SEOSiteTreeExtension.php
+++ src/Extensions/SEOSiteTreeExtension.php
@@ -59,7 +59,7 @@ class SEOSiteTreeExtension extends Extension
      * @param  array $tags the original tags
      * @return array
      */
-    public function MetaComponents(&$tags)
+    public function updateMetaComponents(&$tags)
     {
         /** @var SiteTree $owner */
         $owner = $this->getOwner();
@@ -71,8 +71,6 @@ class SEOSiteTreeExtension extends Extension
         // We respect the "ShowInSearch" Setting for SiteTree objects, for everything
         // else we assume a free pass
         if ($source->ShowInSearch && $source instanceof SiteTree) {
-            throw new \Exception("Error Processing Request", 1);
-
             $tags['robots'] = [
                 'tag' => 'meta',
                 'attributes' => [
@@ -89,6 +87,8 @@ class SEOSiteTreeExtension extends Extension
                 ],
             ];
         } else {
+            throw new \Exception("Error Processing Request", 1);
+
             $tags['robots'] = [
                 'tag' => 'meta',
                 'attributes' => [
